7e4d87d3f06a5570726354dbb9829dccadad1bcb
WilmerSoriano <Official_Wilmer.2020@outlook.com>
2025-01-04 01:27:48 -0600

    fixing menu rendering

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { StrictMode } from 'react'
+import ReactDOM from "react-dom/client"
 import App from './App.jsx'
 import './index.css'
 
 import "@fontsource/outfit"
 import "@fontsource/roboto"
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
