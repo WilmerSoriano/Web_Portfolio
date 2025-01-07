87069d2517e8c9ca90d9e9abee8c259dfe9a637a
WilmerSoriano <Official_Wilmer.2020@outlook.com>
2025-01-07 15:10:29 -0600

    added contact Information

---
+++ Portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { getImageUrl } from "../../utils";
+import styles from "./Contact.module.css";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:official_Wilmer.2020@outlook.com">official_Wilmer.2020@outlook.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https:www.linkedin.com/in/wilmer-s-a37b18264">www.linkedin.com/in/wilmer</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/WilmerSoriano">https://github.com/WilmerSoriano</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
