ed21936238b7587b01f819bef4ef653da2c47d46
WilmerSoriano <Official_Wilmer.2020@outlook.com>
2025-01-07 14:42:35 -0600

    Added Project jsx and Project card and updated some project comments

--- Portfolio/src/components/Projects/Projects.jsx
+++ Portfolio/src/components/Projects/Projects.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import projects from "../../data/projects.json";
+import { ProjectCard } from "./ProjectCard";
+import styles from "./Projects.module.css";
 
 export const Projects = () => {
-   return <div>Projects</div>
-}
+  return (
+    <section className={styles.container} id="projects">
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.projects}>
+        {projects.map((project, id) => {
+          return <ProjectCard key={id} project={project} />;
+        })}
+      </div>
+    </section>
+  );
+};
